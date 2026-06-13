c596b092ff8b6f28cd7b1ad8b1781dd4893560cb
Kurtis Knodel <kurtis@kurtisknodel.com>
2026-06-12 18:44:06 -0700

    Add asRows

--- src/matrices.zig
+++ src/matrices.zig
@@ -363,6 +363,17 @@ pub fn Mat4(N: type) type {
 
         // --- METHODS ---
 
+        /// Returns the rows of this matrix.
+        pub fn asRows(self: Self) [4]@Vector(4, N) {
+            const m = self.m;
+            return .{
+                .{ m[0], m[4], m[8], m[12] },
+                .{ m[1], m[5], m[9], m[13] },
+                .{ m[2], m[6], m[10], m[14] },
+                .{ m[3], m[7], m[11], m[15] },
+            };
+        }
+
         /// Returns true if `self` is exactly equal to `other`.
         pub fn eql(self: Self, other: Self) bool {
             return std.mem.eql(N, &self.m, &other.m);
